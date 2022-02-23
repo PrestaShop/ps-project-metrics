0b9624b10f2cf418e6588a112ce670e17984618a
matks <mathieu.ferment@prestashop.com>
2022-02-23 14:14:17 +0100

    Persist main review msg

--- src/Controller/ReceiveGitHubWebHookController.php
+++ src/Controller/ReceiveGitHubWebHookController.php
@@ -42,28 +42,51 @@ class ReceiveGitHubWebHookController extends AbstractController
         if (!$json) {
             return new Response();
         }
-        
-        if (!isset($json['comment'])
-            || !isset($json['comment']['user'])
-            || !isset($json['repository'])) {
+
+        $isAPRReviewComment = isset($json['comment'])
+            && isset($json['comment']['user'])
+            && isset($json['repository']);
+        $isAMainPRReviewComment = isset($json['review']);
+
+        if (!$isAPRReviewComment && !$isAMainPRReviewComment) {
             return new Response();
         }
 
-        if (!in_array($json['comment']['user']['login'], TeamHelper::getTeam())) {
+        if ($isAPRReviewComment) {
+            $author = $json['comment']['user']['login'];
+        } else {
+            $author = $json['review']['user']['login'];
+        }
+
+        if (!in_array($author, TeamHelper::getTeam())) {
             return new Response();
         }
 
-        $comment = new PRReviewComment(
-            PRReviewComment::TYPE_PR_REVIEW_COMMENT,
-            basename($json['comment']['pull_request_url']),
-            (string)$json['comment']['pull_request_review_id'],
-            $json['comment']['html_url'],
-            $json['pull_request']['html_url'],
-            $json['comment']['body'],
-            $json['comment']['user']['login'],
-            $json['repository']['full_name'],
-            new DateTime($json['comment']['created_at'])
-        );
+        if ($isAPRReviewComment) {
+            $comment = new PRReviewComment(
+                PRReviewComment::TYPE_PR_REVIEW_COMMENT,
+                basename($json['comment']['pull_request_url']),
+                (string)$json['comment']['pull_request_review_id'],
+                $json['comment']['html_url'],
+                $json['pull_request']['html_url'],
+                $json['comment']['body'],
+                $author,
+                $json['repository']['full_name'],
+                new DateTime($json['comment']['created_at'])
+            );
+        } else {
+            $comment = new PRReviewComment(
+                PRReviewComment::TYPE_PR_REVIEW_MAIN_MESSAGE,
+                (string)$json['pull_request']['number'],
+                (string)$json['review']['id'],
+                $json['review']['html_url'],
+                $json['pull_request']['html_url'],
+                $json['review']['body'],
+                $author,
+                $json['repository']['full_name'],
+                new DateTime($json['review']['submitted_at'])
+            );
+        }
 
         $this->entityManager->persist($comment);
         $this->entityManager->flush();
